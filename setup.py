e3559d0e86589a17d70b9e3fbfb341337d1564b7
gitronald <gitronald@users.noreply.github.com>
2019-12-05 16:38:20 -0500

    Bump version: 0.1.12 → 0.1.13

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ description, long_description = get_readme_descriptions()
 
 setuptools.setup(
     name='WebSearcher',
-    version='0.1.12',
+    version='0.1.13',
     url='http://github.com/gitronald/WebSearcher',
     author='Ronald E. Robertson',
     author_email='rer@ccs.neu.edu',
