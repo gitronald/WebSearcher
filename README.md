166923ed5ff103157eb4a2f1713ac779c1b5ae20
gitronald <gitronald@users.noreply.github.com>
2019-12-10 18:09:37 -0500

    Bump version: 0.1.13 → 0.1.14

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WebSearcher 0.1.13  
+# WebSearcher 0.1.14  
 ## Tools for conducting, collecting, and parsing web search  
 [![PyPI version](https://badge.fury.io/py/WebSearcher.svg)](https://badge.fury.io/py/WebSearcher)
 
--- WebSearcher/__init__.py
+++ WebSearcher/__init__.py
@@ -12,7 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-__version__ = "0.1.13"
+__version__ = "0.1.14"
 from .searchers import SearchEngine
 from .parsers import parse_serp, extract_components
 from .locations import download_locations
