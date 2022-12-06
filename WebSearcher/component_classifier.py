d91e863f829eae040477114608dd8ef7e0691a27
gitronald <gitronald@users.noreply.github.com>
2022-12-06 01:31:33 -0800

    add: functions for complicated classifiers, additional header text checks

--- WebSearcher/component_classifier.py
+++ WebSearcher/component_classifier.py
@@ -1,41 +1,7 @@
-# Classifications based on H2 Headings
-h2_matches = {
-    "Featured snippet from the web": "knowledge",
-    "Unit Converter": "knowledge",
-    "Sports Results": "knowledge",
-    "Weather Result": "knowledge",
-    "Finance Results": "knowledge",
-    "Calculator Result": "knowledge",
-    "Translation Result": "knowledge",
-    "Resultado de traducción": "knowledge",
-    "Knowledge Result": "knowledge",
-    "Jobs": "jobs",
-    "Web results": "general",
-    "Resultados de la Web": "general",
-    "Web Result with Site Links": "general",
-    "Local Results": "local_results",
-    "Map Results": "map_results",
-    "People also ask": "people_also_ask",
-    "Twitter Results": "twitter",
-}
+"""SERP component classifiers
+"""
 
-# Classifications based on H3 Headings
-h3_matches = {
-    "Videos": "videos",
-    "Top stories": "top_stories",
-    "Quotes in the news": "news_quotes",
-    "Latest from": "latest_from",
-    "View more videos": "view_more_videos",
-    "View more news": "view_more_news",
-    "Images for": "images",
-    "Scholarly articles for": "scholarly_articles",
-    "Recipes": "recipes",
-    "Popular products": "products",
-    "Related searches": "searches_related",
-}
-
-
-def classify_type(cmpt, cmpt_type="unknown"):
+def classify_type(cmpt):
     """Component classifier
 
     Args:
@@ -44,10 +10,9 @@ def classify_type(cmpt, cmpt_type="unknown"):
     Returns:
         str: A classification of the component type
     """
-
-    # Define HTML references (sets to `None` if it doesn't exist)
-    h2 = cmpt.find("h2")
-    h3 = cmpt.find("h3")
+    
+    # Default unknown
+    cmpt_type = "unknown"
     g_tray = cmpt.find("g-tray-header")
     g_section = cmpt.find("g-section-with-header")
     carousel = cmpt.find("g-scrolling-carousel")
@@ -59,11 +24,11 @@ def classify_type(cmpt, cmpt_type="unknown"):
     hybrid = cmpt.find("div", {"class": "ifM9O"})
     twitter = cmpt.find_previous().text == "Twitter Results"
 
-    if "class" in cmpt.attrs.keys() and cmpt.attrs["class"][0] == "hlcw0c":
+    if "class" in cmpt.attrs and cmpt.attrs["class"][0] == "hlcw0c":
         cmpt_type = "general"
 
-    # Checks a g-scrolling-carousel for a specific id to classify as top stories as not all
-    # top stories have an h3 tag
+    # Checks a g-scrolling-carousel for a specific id to classify as not all 
+    # top_stories have an h3 tag
     if carousel:
         if "class" in carousel.attrs and carousel.attrs["class"][0] == "F8yfEe":
             cmpt_type = "top_stories"
@@ -71,22 +36,16 @@ def classify_type(cmpt, cmpt_type="unknown"):
     # Check component header
     cmpt_header = cmpt.find("div", {"class": "mfMhoc"})
     if cmpt_header:
-        for text, ctype in h3_matches.items():
+        for text, ctype in h3_text_to_label.items():
             if cmpt_header.text.startswith(text):
                 cmpt_type = ctype
 
-    # Check `h2.text` for string matches
-    if h2:
-        for text, ctype in h2_matches.items():
-            if h2.text.startswith(text):
-                cmpt_type = ctype
-
-    # Check `h3.text` for string matches
-    if h3:
-        for text, ctype in h3_matches.items():
-            if h3.text.startswith(text):
-                cmpt_type = ctype
-
+    if cmpt_type == "unknown":
+        cmpt_type = classify_h2_divs(cmpt)
+        
+    if cmpt_type == "unknown":
+        cmpt_type = classify_h3_divs(cmpt)
+                
     # Twitter subtype
     if twitter or cmpt_type == "twitter":
         cmpt_type = "twitter_cards" if carousel else "twitter_result"
@@ -107,41 +66,127 @@ def classify_type(cmpt, cmpt_type="unknown"):
         cmpt_type = "available_on"
 
     # Check if component is only of class 'g'
-    if "class" in cmpt.attrs:
-        if cmpt.attrs["class"] == ["g"]:
-            cmpt_type = "general"
+    if "class" in cmpt.attrs and cmpt.attrs["class"] == ["g"]:
+        cmpt_type = "general"
 
     # check for people also ask
     if cmpt_type == "unknown":
-        if "class" in cmpt.attrs.keys() and cmpt.attrs["class"] == [
-            "g",
-            "kno-kp",
-            "mnr-c",
-            "g-blk",
-        ]:
-            cmpt_type = "people_also_ask"
+        cmpt_type = classify_people_also_ask(cmpt)
 
     # check for flights, maps, hotels, events, jobs
     if cmpt_type == "unknown":
-        if (
-            "jscontroller" in cmpt.attrs.keys()
-            and cmpt.attrs["jscontroller"] == "Z2bSc"
-        ):
-            cmpt_type = "flights"
-        elif cmpt.find("div", {"jscontroller": "Z2bSc"}):
-            cmpt_type = "flights"
-        elif "data-hveid" in cmpt.attrs.keys() and cmpt.attrs["data-hveid"] == "CAMQAA":
-            cmpt_type = "maps"
-        elif cmpt.find("div", {"class": "zd2Jbb"}):
-            cmpt_type = "hotels"
-        elif cmpt.find("g-card", {"class": "URhAHe"}):
-            cmpt_type = "events"
-        elif cmpt.find("g-card", {"class": "cvoI5e"}):
-            cmpt_type = "jobs"
-        else:
-            cmpt_text = [text for text in cmpt.stripped_strings]
-            if len(cmpt_text) >= 1 and cmpt_text[0] == "COVID-19 alert":
-                cmpt_type = "covid_alert"
+        cmpt_type = classify_knowledge_box(cmpt)
 
     # Return type or unknown (default)
     return cmpt_type
+
+
+def check_dict_value(d, key, value):
+    """Check if a key exists in a dictionary and is equal to a input value"""
+    return (d[key] == value) if key in d else False
+
+
+# Classifications based on H2 Headings
+h2_text_to_label = {
+    "Featured snippet from the web": "knowledge",
+    "Unit Converter": "knowledge",
+    "Sports Results": "knowledge",
+    "Weather Result": "knowledge",
+    "Finance Results": "knowledge",
+    "Calculator Result": "knowledge",
+    "Translation Result": "knowledge",
+    "Resultado de traducción": "knowledge",
+    "Knowledge Result": "knowledge",
+    "Jobs": "jobs",
+    "Web results": "general",
+    "Resultados de la Web": "general",
+    "Web Result with Site Links": "general",
+    "Local Results": "local_results",
+    "Map Results": "map_results",
+    "People also ask": "people_also_ask",
+    "Twitter Results": "twitter",
+}
+
+def classify_h2_divs(cmpt):
+    """Check h2 text for string matches"""
+
+    h2_list = [
+        cmpt.find("h2"),
+        cmpt.find("div", {'aria-level':"2", "role":"heading"})
+    ]
+
+   # Check `h2.text` for string matches
+    for h2 in filter(None, h2_list):
+        for text, label in h2_text_to_label.items():
+            if h2.text.startswith(text):
+                return label
+    return "unknown"
+
+
+# Classifications based on H3 Headings
+h3_text_to_label = {
+    "Videos": "videos",
+    "Top stories": "top_stories",
+    "Quotes in the news": "news_quotes",
+    "Latest from": "latest_from",
+    "View more videos": "view_more_videos",
+    "View more news": "view_more_news",
+    "Images for": "images",
+    "Scholarly articles for": "scholarly_articles",
+    "Recipes": "recipes",
+    "Popular products": "products",
+    "Related searches": "searches_related",
+}
+
+def classify_h3_divs(cmpt):
+    """Check h3 text for string matches"""
+    
+    h3_list = [
+        cmpt.find("h3"),
+        cmpt.find("div", {'aria-level':"3", "role":"heading"})
+    ]
+
+    for h3 in filter(None, h3_list):
+        for text, label in h3_text_to_label.items():
+            if h3.text.startswith(text):
+                return label
+    return "unknown"
+
+
+def classify_people_also_ask(cmpt):
+    class_list = ["g", "kno-kp", "mnr-c", "g-blk"]
+    conditions = check_dict_value(cmpt.attrs, "class", class_list)
+    return 'people_also_ask' if conditions else "unknown"
+
+
+def classify_knowledge_box(cmpt):
+    """Classify knowledge component types
+    
+    Creates conditions for each label in a dictionary then assigns the 
+    label as the component type if it's conditions are met using one, or some 
+    combination, of the following three methods:
+
+    1. Check if a component has a specific attribute value
+    2. Check if a component contains a specific div
+    3. Check if a component has a matching string in its text
+    
+    """
+    attrs = cmpt.attrs
+    text_list = list(cmpt.stripped_strings)
+
+    condition = {}
+    condition['flights'] = (
+        (check_dict_value(attrs, "jscontroller", "Z2bSc")) |
+        bool(cmpt.find("div", {"jscontroller": "Z2bSc"}))
+    )
+    condition['maps'] = check_dict_value(attrs, "data-hveid", "CAMQAA")
+    condition['hotels'] = cmpt.find("div", {"class": "zd2Jbb"})
+    condition['events'] = cmpt.find("g-card", {"class": "URhAHe"})
+    condition['jobs'] = cmpt.find("g-card", {"class": "cvoI5e"})
+    condition['covid_alert'] = (
+        (len(text_list) >= 1) & 
+        (text_list[0] == "COVID-19 alert")
+    )
+
+    for condition_type, conditions in condition.items():
+        return condition_type if conditions else "unknown"
