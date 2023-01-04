e15add8d659781a4d7df5a052b65bff74c62bf1d
gitronald <gitronald@users.noreply.github.com>
2023-01-04 11:25:43 -0800

    fix: ad url parsing

--- WebSearcher/component_parsers/ads.py
+++ WebSearcher/component_parsers/ads.py
@@ -16,14 +16,26 @@ def parse_ads(cmpt):
 
     return [parser(sub, sub_rank) for sub_rank, sub in enumerate(subs)]
 
+
+def get_link(soup, kwargs, key='href'):
+    """Utility for soup.find('a')['href'] that checks if 'a' exists and has an href key"""
+    a = soup.find('a', kwargs)
+    return a.attrs[key] if a.attrs and key in a.attrs else None
+
+
+def get_text(soup, div, kwargs):
+    """Utility for soup.find('div', ...).text that checks if div exists"""
+    div = soup.find(div, kwargs)
+    return div.text if div else None
+    
+
 def parse_ad(sub, sub_rank=0, visible=None):
     """Parse details of a single ad subcomponent, similar to general"""
-    parsed = {'type':'ad', 'sub_rank':sub_rank}
+    parsed = {'type':'ad', 'sub_rank': 0}
 
-    parsed['title'] = sub.find('div', {'role':'heading'}).text
-    parsed['url'] = sub.find('div', {'class':'d5oMvf'}).find('a')['href']
-    if sub.find('span', {'class':'Zu0yb'}):
-        parsed['cite'] = sub.find('span', {'class':'Zu0yb'}).text
+    parsed['title'] = get_text(sub, 'div', {'role':'heading'})
+    parsed['url'] = get_link(sub, {"class":"sVXRqc"})
+    parsed['cite'] = get_text(sub, 'span', {'class':'Zu0yb'})
 
     # Take the top div with this class, should be main result abstract
     text_divs = sub.find_all('div', {'class':'yDYNvb'})
@@ -35,8 +47,8 @@ def parse_ad(sub, sub_rank=0, visible=None):
         if list_items:
             alinks = [i.find('a') for i in list_items]
             parsed['details'] = [a['href'] for a in alinks if a]
-    
     return parsed
+
 
 def parse_ad_secondary(sub, sub_rank=0, visible=None):
     """Parse details of a single ad subcomponent, similar to general"""
