c46f499a1fcac5d68f187eb3151dbdd9cb06eaa8
gitronald <gitronald@users.noreply.github.com>
2023-03-27 15:32:11 -0700

    Bump to 2023.01.27-b

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ description, long_description = get_readme_descriptions()
 
 setuptools.setup(
     name='WebSearcher',
-    version='2023.01.27-a',
+    version='2023.01.27-b',
     url='http://github.com/gitronald/WebSearcher',
     author='Ronald E. Robertson',
     author_email='rer@ccs.neu.edu',
