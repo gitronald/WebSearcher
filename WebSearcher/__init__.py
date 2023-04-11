a8d93a5865dc59f6b6a5e1fc1dd5d95c43f14618
gitronald <gitronald@users.noreply.github.com>
2023-04-10 19:24:27 -0700

    Bump to 2023.01.27-d

--- WebSearcher/__init__.py
+++ WebSearcher/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2023.01.27-c"
+__version__ = "2023.01.27-d"
 from .searchers import SearchEngine
 from .parsers import parse_serp, extract_components
 from .locations import download_locations
