9b931af0302ebc1a5581e279bb4bcb2cfcfca158
gitronald <gitronald@users.noreply.github.com>
2023-10-16 15:28:36 -0700

    update: modernize user agent string

--- WebSearcher/searchers.py
+++ WebSearcher/searchers.py
@@ -19,7 +19,7 @@ DEFAULT_HEADERS = {
     'Accept': '*/*',
     'Accept-Encoding': 'gzip,deflate,br',
     'Accept-Language': 'en-US,en;q=0.5',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0',
 }
 
 def utc_stamp(): return datetime.utcnow().isoformat()
