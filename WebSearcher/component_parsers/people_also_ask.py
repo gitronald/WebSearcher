64d01fa88b3a808a80f63879068d4b562e674a2b
gitronald <gitronald@users.noreply.github.com>
2023-11-17 10:44:24 -0800

    update: people also ask, use model

--- WebSearcher/component_parsers/people_also_ask.py
+++ WebSearcher/component_parsers/people_also_ask.py
@@ -1,4 +1,5 @@
 from .. import webutils
+from ..models import BaseResult
 
 def parse_people_also_ask(cmpt, sub_rank=0):
     """Parse a "People Also Ask" component
@@ -14,34 +15,35 @@ def parse_people_also_ask(cmpt, sub_rank=0):
     Returns:
         list : list of parsed subcomponent dictionaries
     """
-    parsed = {'type':'people_also_ask', 'sub_rank':sub_rank}
+    parsed = BaseResult(
+        type='people_also_ask',
+        sub_rank=sub_rank,
+    )
     # questions = cmpt.find_all('g-accordion-expander')
     # questions = cmpt.find('section').find_all('div', {'class':'yTrXHe'})
     questions = cmpt.find_all("div", {"class":"related-question-pair"})
+    parsed.details = [parse_question(q) for q in questions] if questions else None
 
-    parsed['details'] = [parse_question(q) for q in questions] if questions else None
-    return [parsed]
+    return [parsed.model_dump()]
+
 
 def parse_question(question):
     """Parse an individual question in a "People Also Ask" component"""
     
-    alinks = question.find_all('a')
-    if not alinks:
-        return None
-    
     # Get query and URL fragments
     parsed = {
-        'qry': webutils.get_text(alinks[-1]),
-        'qry_url': alinks[-1]['href'],
+        'title': None,
+        'url': None,
     }
 
     # Get title
     title_divs = [
         question.find('div', {'class':'rc'}),
-        question.find('div', {'class':'yuRUbf'})
+        question.find('div', {'class':'yuRUbf'}),
+        question.find('div', {'class': 'JlqpRe'}),  # 2023-11-16
     ]
     for title_div in filter(None, title_divs):
-        parsed['title'] = webutils.get_text(title_div, 'h3')
+        parsed['title'] = webutils.get_text(title_div)
         parsed['url'] = webutils.get_link(title_div)
 
     # Get citation
@@ -56,3 +58,5 @@ def parse_question(question):
     parsed['text'] = text if text else None
 
     return parsed
+
+
