2dd8b953c834b61851d5fd56e950592a5ce59b15
gitronald <gitronald@users.noreply.github.com>
2023-11-17 16:12:07 -0800

    update: use models, modern SERPs

--- WebSearcher/component_parsers/map_results.py
+++ WebSearcher/component_parsers/map_results.py
@@ -1,3 +1,6 @@
+from .. import webutils 
+from ..models import BaseResult
+
 def parse_map_results(cmpt, sub_rank=0):
     """Parse a "Map Results" component
 
@@ -10,17 +13,13 @@ def parse_map_results(cmpt, sub_rank=0):
     Returns:
         dict : parsed result
     """
-    parsed = {'type':'map_results', 'sub_rank':sub_rank}
-    details = {}
+    parsed = BaseResult(
+        type='map_results',
+        sub_rank=sub_rank,
+        title=get_title(cmpt)
+    )
+    return [parsed.model_dump()]
 
-    title_div = cmpt.find('div', {'class':'desktop-title-content'})
-    details['title'] = title_div.text if title_div else None
-
-    subtitle_span = cmpt.find('span', {'class':'desktop-title-subcontent'})
-    details['subtitle'] = subtitle_span.text if subtitle_span else None
-
-    img = cmpt.find('img', {'id':'lu_map'})
-    if img:
-        details['img_title'] = img.attrs['title'] if 'title' in img.attrs else None
-    parsed['details'] = details
-    return [parsed]
+def get_title(cmpt):
+    # return webutils.get_text(cmpt, 'div', {'class':'desktop-title-content'})
+    return webutils.get_text(cmpt, 'div', {'class':'aiAXrc'})
