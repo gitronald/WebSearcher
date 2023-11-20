4960db82ec9e99b49ae17298e06404659c881989
gitronald <gitronald@users.noreply.github.com>
2023-11-20 14:15:47 -0800

    update: switch main init to .extractors

--- WebSearcher/__init__.py
+++ WebSearcher/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "0.3.4"
 from .searchers import SearchEngine
-from .parsers import parse_serp, extract_components
+from .parsers import parse_serp
+from .extractors import extract_components
 from .locations import download_locations
 from .component_classifier import classify_type
 from .webutils import load_html, make_soup, load_soup
