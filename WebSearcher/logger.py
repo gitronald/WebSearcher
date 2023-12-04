3444a176945061444dc4bb1bdce28fa1741167ea
gitronald <gitronald@users.noreply.github.com>
2023-12-04 11:46:09 -0800

    update: more logger args and docs

--- WebSearcher/logger.py
+++ WebSearcher/logger.py
@@ -1,53 +1,69 @@
 """ Configure a logger using a dictionary
 """
 
-import sys
 import logging.config
-from datetime import datetime
+from typing import Optional
 
 # Formatters: change what gets logged
 minimal = '%(message)s'
-detailed  = '%(asctime)s | %(process)d | %(levelname)s | %(name)s | %(message)s'
-formatters = { 
+medium = '%(asctime)s.%(msecs)01d | %(levelname)s | %(name)s | %(message)s'
+detailed = '%(asctime)s.%(msecs)01d | %(process)d | %(levelname)s | %(name)s | %(message)s'
+formatters = {
     'minimal': {'format': minimal},
-    'detailed': {'format': detailed}
+    'medium': {'format': medium, 'datefmt': '%Y-%m-%d %H:%M:%S'},
+    'detailed': {'format': detailed, 'datefmt': '%Y-%m-%d %H:%M:%S'}
 }
 
-class Logger(object):
-    """ Get logger and set console and file outputs
-
-        Ex: 
-        ```
-        from logger import Summary
-        log = Logger('summary.log').get_logger('mylogger')
-         
-        ```
+class Logger:
     """
-    def __init__(self, 
-        file_name='',  file_mode='w', file_format='detailed',
-        console=True, console_format='detailed', console_level='DEBUG'):
+    A configurable logger for console and file outputs.
+
+    Attributes:
+        log_config (dict): Configuration dictionary for the logger.
+
+    Methods:
+        start(name: Optional[str]): Initializes and retrieves the logger instance.
+    """
+
+    def __init__(self,
+                 console: bool = True, 
+                 console_format: str = 'medium', 
+                 console_level: str = 'DEBUG',                
+                 file_name: str = '',  
+                 file_mode: str = 'w', 
+                 file_format: str = 'detailed',
+                 file_level: str = 'INFO') -> None:
+        """
+        Initializes the Logger configuration.
+
+        Args:
+            console (bool): Flag to enable or disable console logging.
+            console_format (str): Format of the console logging. Should be either 'minimal' or 'detailed'.
+            console_level (str): Logging level for the console. Default is 'DEBUG'.
+            file_name (str): Name of the file to log messages. If empty, file logging is disabled.
+            file_mode (str): File mode for file logging. Default is 'w' (write).
+            file_format (str): Format of the file logging. Should be either 'minimal' or 'detailed'.
+            file_level (str): Logging level for the file. Default is 'INFO'.
+        """
     
         # Handlers: change file and console logging details
         handlers = {}
         if console:
             assert console_format in formatters.keys(), \
-                f'Must select formatting type from {list(formatters.keys())}'
-
+                f'Console format must be one of {list(formatters.keys())}'
             handlers['console_handle'] = { 
                 'class': 'logging.StreamHandler',
-                'level': 'DEBUG',
+                'level': console_level,
                 'formatter': console_format,
             }
 
         if file_name:
-            assert type(file_name) is str, \
-                'Must provide name for file logging'
+            assert type(file_name) is str, 'File name must be a string'
             assert file_format in formatters.keys(), \
-                f'Must select formatting type from {list(formatters.keys())}'
-
+                f'File format must be one of {list(formatters.keys())}'
             handlers['file_handle'] = { 
                 'class': 'logging.FileHandler',
-                'level': 'INFO',
+                'level': file_level,
                 'formatter': file_format,
                 'filename': file_name,
                 'mode': file_mode
@@ -77,15 +93,6 @@ class Logger(object):
             'loggers': loggers
         }
         
-    def start(self, name=__name__):
+    def start(self, name: Optional[str] = __name__) -> logging.Logger:
         logging.config.dictConfig(self.log_config)
         return logging.getLogger(name)
-
-    def header(log, title, script=sys.argv[0]):
-        script = script.split('/')[-1].split('.')[0]
-        log.info(f'# {title}\n')
-        log.info(f'By: {script}')
-        log.info(f'On: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
-
-    # def generated_by(logger, scriptname):
-
