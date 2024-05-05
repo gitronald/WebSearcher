b51d395f7fc16971f6fa147066792cd8404c8fc9
gitronald <gitronald@users.noreply.github.com>
2024-05-05 15:53:08 -0700

    update: better error null handling, validate data after parse

--- WebSearcher/component_parsers/twitter_cards.py
+++ WebSearcher/component_parsers/twitter_cards.py
@@ -1,12 +1,14 @@
 from .. import webutils
 from ..models import BaseResult
 
+
 def parse_twitter_cards(cmpt):
     """Parse a Twitter carousel component
 
-    These components contain an carousel of Tweets from an account or about a
-    topic
-    
+    These components consist of a header and a carousel of cards:
+    - header: linked header of a Twitter carousel (account or topic)
+    - card: single Tweet from a Twitter carousel
+
     Args:
         cmpt (bs4 object): A twitter cards component
     
@@ -14,96 +16,54 @@ def parse_twitter_cards(cmpt):
         list : list of parsed subcomponent dictionaries
     """
     # header, carousel = list(cmpt.find('g-section-with-header').children)[:2]
+    parsed_header = parse_twitter_header(cmpt)
     carousel = cmpt.find('g-scrolling-carousel')
-    parsed_list = parse_twitter_header(cmpt)
     subs = carousel.find_all('g-inner-card')
-    parsed_cards = [parse_twitter_card(sub, sub_rank) for sub_rank, sub in enumerate(subs)]
-    parsed_list.extend(parsed_cards)
+    parsed_cards = [parse_twitter_card(sub, sub_rank + 1) for sub_rank, sub in enumerate(subs)]
+    parsed_list = [parsed_header] + parsed_cards
     return parsed_list
 
-def parse_twitter_header(header, sub_rank=0):
-    """Parse the Twitter component header"""
 
-    parsed = BaseResult(
-        type='twitter_cards',
-        sub_type='header',
-        sub_rank=sub_rank,
-        title='',
-        url='',
-        cite=webutils.get_text(header, 'cite')
-    )
+def parse_twitter_header(cmpt, sub_rank:int = 0):
+    """Parse a Twitter header from the main component"""
+    parsed = {"type": "twitter_cards", 
+              "sub_type": "header", 
+              "sub_rank": sub_rank}
+    element_current = cmpt.find('g-link')
+    element_legacy = cmpt.find('h3', {'class':'r'})
+    if cmpt.find('h3'):
+        if element_legacy:
+            parsed['url'] = webutils.url_unquote(element_legacy.get('href', ''))
+            parsed['title'] = webutils.get_text(element_legacy, 'a')
+        elif element_current:
+            parsed['url'] = webutils.url_unquote(webutils.get_link(element_current))
+            parsed['title'] = webutils.get_text(element_current)
+    elif element_current:
+        parsed['url'] = webutils.get_link(element_current)
+        parsed['title'] = webutils.get_text(element_current)
+    parsed["cite"] = webutils.get_text(cmpt, 'cite')
 
-    header_details = get_header_details(header)
-    parsed.title = header_details['title']
-    parsed.url = header_details['url']
-
-    return [parsed.model_dump()]
-
-
-def get_header_details(header):
-    """Handle legacy and current formats"""
-
-    if header.find('h3'):
-
-        if header.find('h3', {'class':'r'}):
-            # Legacy format
-            element = header.find('h3', {'class':'r'})
-            header_details = {
-                'url': webutils.url_unquote(element['href']), 
-                'title': webutils.get_text(element, 'a')
-            }
-        else:
-            # Current
-            glink = header.find('g-link')
-            header_details = {
-                'url': webutils.url_unquote(glink.a['href']) if glink else '', 
-                'title': webutils.get_text(glink)
-            }
-    else:
-        glink = header.find('g-link')
-        header_details = {
-            'url': webutils.get_link(glink) if glink else '', 
-            'title': webutils.get_text(glink)
-        }
-    return header_details
+    validated = BaseResult(**parsed)
+    return validated.model_dump()
 
 
-def parse_twitter_card(sub, sub_rank=0):
-    """Parse a Twitter cards subcomponent
-    
-    Args:
-        sub (bs4 object): A local results subcomponent
-    
-    Returns:
-        dict : parsed subresult
-    """
-    parsed = BaseResult(
-        type='twitter_cards',
-        sub_type='card',
-        sub_rank=sub_rank + 1,  # Add one to rank to account for header
-    )
+def parse_twitter_card(sub, sub_rank:int = 0):
+    """Parse a Twitter card from a subcomponent"""
+    parsed = {"type": "twitter_cards",
+              "sub_type": "card",
+              "sub_rank": sub_rank}
 
     # Tweet account
     title = sub.find('g-link')
-    if title:
-        parsed.title = webutils.get_text(title, 'a')
+    parsed["title"] = webutils.get_text(title, 'a') if title else None
 
-    # Bottom div containing timestamp and tweet link
+    # Bottom div containing details
     div = sub.find('div', {'class':'Brgz0'})
     if div:
-        parsed.cite = webutils.get_text(div, 'div', {'class':'rmxqbe'})
-        
         url = webutils.get_link(div)
-        parsed.url = webutils.url_unquote(url) if url else None
-        parsed.text = webutils.get_text(div, 'div', {'class':'xcQxib'})
-    return parsed.model_dump()
+        parsed["url"] = webutils.url_unquote(url) if url else None
+        parsed["text"] = webutils.get_text(div, 'div', {'class':'xcQxib'})
+        parsed["cite"] = webutils.get_text(div, 'div', {'class':'rmxqbe'})
 
-# Deprecated: text processing should be done post-parse
-# def get_details(div):
-#     details = {}
-#     post_content = div.find('div', {'class':'xcQxib'})
-#     links = [a for a in post_content.find_all('a') if 'href' in a.attrs]
-#     details['urls'] = [webutils.url_unquote(a['href']) for a in links]
-#     details['hashtags'] = webutils.parse_hashtags(parsed.text)
-#     # details['emojis'] = webutils.parse_emojis(parsed['text'])
-#     return details
+    validated = BaseResult(**parsed)
+    return validated.model_dump()
