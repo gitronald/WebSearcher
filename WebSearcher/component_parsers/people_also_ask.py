8e05e6b839262f494f8f453818be8a05be74a0dc
gitronald <gitronald@users.noreply.github.com>
2025-01-16 13:10:29 -0800

    fix: text unbound error in ppa component circa Jan 2021

--- WebSearcher/component_parsers/people_also_ask.py
+++ WebSearcher/component_parsers/people_also_ask.py
@@ -18,28 +18,32 @@ def parse_people_also_ask(cmpt, sub_rank=0) -> list:
     # questions = cmpt.find_all('g-accordion-expander')
     # questions = cmpt.find('section').find_all('div', {'class':'yTrXHe'})
     questions = cmpt.find_all("div", {"class":"related-question-pair"})
-    details = [parse_question(q) for q in questions] if questions else None
-
+    parsed_questions = [parse_question(q) for q in questions]
+    parsed_questions = filter(None, parsed_questions)
     parsed = {
         'type': 'people_also_ask',
         'sub_rank': sub_rank,
-        'text': "<|>".join(details) if details else None,
-        'details': details,
+        'text': "<|>".join(parsed_questions) if parsed_questions else None,
+        'details': parsed_questions if parsed_questions else None
     }
-
     return [parsed]
 
 
 def parse_question(question):
     """Parse an individual question in a "People Also Ask" component"""
-
-    # Get title
+    
     title_divs = [
         question.find('div', {'class':'rc'}),
         question.find('div', {'class':'yuRUbf'}),
         question.find('div', {'class':'iDjcJe'}),  # 2023-01-01
         question.find('div', {'class':'JlqpRe'}),  # 2023-11-16
+        question.find('div', {'class':'cbphWd'}),  # 2021-01-09
     ]
+    
+    # Return first valid text found
     for title_div in filter(None, title_divs):
-        text = webutils.get_text(title_div)
-    return text if text else None
+        text = webutils.get_text(title_div, strip=True)
+        if text:
+            return text
+    
+    return None
