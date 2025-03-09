ea6eebc9850319447cff8cfd516f90e6887e0b54
gitronald <gitronald@users.noreply.github.com>
2025-03-09 14:21:27 -0700

    update: specify args, headless not working locally

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -1,6 +1,11 @@
 import WebSearcher as ws
+
+#chromedriver_path = "/opt/homebrew/Caskroom/chromedriver/133.0.6943.53"
+
 se = ws.SearchEngine()                     # 1. Initialize collector
-se.launch_chromedriver(headless=False, version_main=133)   # 2. Launch undetected chromedriver window
+se.launch_chromedriver(headless=False,     # 2. Launch undetected_chromedriver window 
+                       use_subprocess=False,
+                       version_main=133)   
 se.search('immigration news')              # 2. Conduct a search
 se.parse_results()                         # 3. Parse search results
 se.save_serp(append_to='serps.json')       # 4. Save HTML and metadata
