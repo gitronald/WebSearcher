165f9e36a4770b081787d71e10c571fd8c73b16b
gitronald <gitronald@users.noreply.github.com>
2025-03-26 10:25:47 -0700

    update: condense args, use currently reliable default query

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -10,29 +10,26 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    query: str = typer.Argument(..., help="Search query to use"),
+    query: str = typer.Argument("why is the sky blue?", help="Search query to use"),
     method: str = typer.Argument("selenium", help="Search method to use: 'selenium' or 'requests'"),
     headless: bool = typer.Option(False, help="Run browser in headless mode"),
     use_subprocess: bool = typer.Option(False, help="Run browser in a separate subprocess"),
     version_main: int = typer.Option(133, help="Main version of Chrome to use"),
-    ai_expand: bool = typer.Option(False, help="Expand AI overviews if present"),
-    driver_executable_path: str = typer.Option(None, help="Path to ChromeDriver executable"),
+    ai_expand: bool = typer.Option(True, help="Expand AI overviews if present"),
+    driver_executable_path: str = typer.Option("", help="Path to ChromeDriver executable"),
     output_prefix: str = typer.Option("output", help="Prefix for output files")
 ) -> None:
 
     typer.echo(f"query: {query}\nmethod: {method}")
-    selenium_config = {
-        "headless": headless,
-        "use_subprocess": use_subprocess,
-        "driver_executable_path": driver_executable_path,
-        "version_main": version_main,
-    }
-    
     se = ws.SearchEngine(
         method=method,
-        selenium_config=selenium_config
+        selenium_config={
+            "headless": headless,
+            "use_subprocess": use_subprocess,
+            "driver_executable_path": driver_executable_path,
+            "version_main": version_main,
+        }
     )
-    
     se.search(qry=query, ai_expand=ai_expand)
     se.parse_results()
     
